78153108a5f94328fad77ce76987bfa66533630e
chatzoudd <pchatzou@gmail.com>
2018-04-24 20:39:57 +0200

    monty pythons!

---
+++ monty.py
@@ -0,0 +1,12 @@
+#encoding: utf-8
+import webbrowser
+
+def watch_monty_pythons():
+
+    """Watch the holy grail on YouTube!"""
+    url = 'https://www.youtube.com/watch?v=JEbeNumBvA0'
+    webbrowser.open(url, new=1)
+
+if __name__ == "__main__":
+
+    watch_monty_pythons()
